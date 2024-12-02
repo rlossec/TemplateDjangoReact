372d781a35f0478bde9b561d883f7a0814da29f5
rlossec <raphaelossec@gmail.com>
2024-12-02 15:43:19 +0100

    Tests for endpoints : reset username and reset password

--- src/backend/accounts/tests/template_error_message.py
+++ src/backend/accounts/tests/template_error_message.py
@@ -2,6 +2,7 @@ MANDATORY_FIELD_ERROR_MESSAGE = 'This field is required.'
 INVALID_USER_ERROR_MESSAGE = "Invalid user id or user doesn't exist."
 INVALID_TOKEN_ERROR_MESSAGE = "Invalid token for given user."
 INVALID_EMAIL_ERROR_MESSAGE = 'Enter a valid email address.'
+INVALID_USERNAME = 'Enter a valid username. This value may contain only letters, numbers, and @/./+/-/_ characters.'
 INVALID_CREDENTIALS_ERROR_MESSAGE = 'No active account found with the given credentials'
 EXPIRED_TOKEN_ERROR_MESSAGE = 'Token is invalid or expired'
 USERNAME_ALREADY_TAKEN_ERROR_MESSAGE = 'A user with that username already exists.'
